27e58e6fb800e80a71539a75c514cbf189aece88
rqy1994 <rqy1994@foxmail.com>
2016-08-10 21:15:20 +0800

    added explanation to insertion_sort.

--- sort/insertion_sort.cpp
+++ sort/insertion_sort.cpp
@@ -1,3 +1,14 @@
+// 插入排序
+// https://zh.wikipedia.org/wiki/%E6%8F%92%E5%85%A5%E6%8E%92%E5%BA%8F
+// 它的工作原理是通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+//
+// 从第一个元素开始，该元素可以认为已经被排序
+// 取出下一个元素，在已经排序的元素序列中从后向前扫描
+// 如果该元素（已排序）大于新元素，将该元素移到下一位置
+// 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置
+// 将新元素插入到该位置后
+// 重复步骤2~5
+
 #include <iostream>
 #include <cstring>
 #include "Vector.h"
@@ -13,6 +24,20 @@ void Vector::insertion_sort(){
         break;
     }
   }
+}
+
+template<typename T>
+void insertion_sort(T arr[], int len)
+{
+	int i, j;
+	for (i = 1; i < len; ++i) {//总体思想就是将未排序数插入到已经排序的集合中。
+		for (j = i - 1; j >= 0; --j) {
+			if (arr[j] > arr[j + 1])
+				swap(arr[j],arr[j + 1] );
+			else
+				break;
+		}
+	}
 }
 
 int main(){
