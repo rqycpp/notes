8fc47220ef2178cef1b5536cd81e37d547b798e9
rqy1994 <rqy1994@foxmail.com>
2016-08-30 20:03:19 +0800

    added two approaches about leetcode.342

--- leetcode/power-of-four.cpp
+++ leetcode/power-of-four.cpp
@@ -20,4 +20,12 @@ Hint:http://www.cnblogs.com/grandyang/p/5403783.html
          double logRes = log10(num) / log10(4);//换底公式
          return (logRes - int(logRes) == 0);
      }
+     //https://discuss.leetcode.com/topic/42914/1-line-c-solution-without-confusing-bit-manipulations
+     bool isPowerOfFour(int num) {
+       return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
+      //     大于0       码重为1                   是3的倍数
+    }
+    bool isPowerOfFour(int num){
+      return num > 0 && (num & (num - 1) == 0) && (num & 0x55555555) == 0;//4^n 二进制的奇数位为1，且只有一个1
+    }
  };
